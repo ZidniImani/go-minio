ea3041a9bea4df6bcb46a8ed5f6284da3b912c64
Zidni Iman Sholihati <zidni.imani@gmail.com>
2021-12-29 11:13:38 +0700

    add subfolder object list

--- listBucketObject.go
+++ listBucketObject.go
@@ -3,29 +3,34 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/minio/minio-go/v7"
 )
 
 func listBucketObject(minioClient *minio.Client, argsRaw []string) {
 	ctx, cancel := context.WithCancel(context.Background())
-	var bucketName string
+	var bucketName, subFolder string
 
 	// check second arguments
 	if len(argsRaw) > 1 {
-		bucketName = argsRaw[1]
+		if len(strings.Split(argsRaw[1], `/`)) == 1 {
+			subFolder = ""
+			bucketName = argsRaw[1]
+		} else {
+			bucketName = strings.Split(argsRaw[1], `/`)[0]
+			subFolder = argsRaw[1][strings.LastIndex(argsRaw[1], `/`)+1:]
+		}
 	} else {
 		panic("Bucket name not found in params")
 	}
 
 	defer cancel()
 
+	// list object
 	objectCh := minioClient.ListObjects(ctx, bucketName, minio.ListObjectsOptions{
-		// Prefix:    "myprefix",
-		// Recursive: true,
+		Prefix: subFolder + "/",
 	})
-
-	fmt.Printf("Object from bucket %v:\n", bucketName)
 
 	for object := range objectCh {
 		if object.Err != nil {
